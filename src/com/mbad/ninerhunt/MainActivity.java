1f2748d4b93a6acde71c721bfbea4de07af46639
Gipionocheiyort <pandrewsdev@gmail.com>
2014-12-07 02:53:23 -0500

    Facebook Integration
    Still not working...bad app key?

--- src/com/mbad/ninerhunt/MainActivity.java
+++ src/com/mbad/ninerhunt/MainActivity.java
@@ -1,38 +1,108 @@
 package com.mbad.ninerhunt;
 
+import java.util.Arrays;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 
 public class MainActivity extends Activity {
 
 	LocationManager locManager;
 
+	// @Override
+	// protected void onCreate(Bundle savedInstanceState) {
+	// super.onCreate(savedInstanceState);
+	// setContentView(R.layout.activity_main);
+	// locManager = (LocationManager)
+	// getSystemService(Context.LOCATION_SERVICE);
+
+	// ImageButton btnGetHint = (ImageButton)
+	// this.findViewById(R.id.ibLogin);
+	// btnGetHint.setOnClickListener(new View.OnClickListener() {
+	// @Override
+	// public void onClick(View v) {
+	// Intent intObj = new Intent(getBaseContext(), HomeActivity.class);
+	// startActivity(intObj);
+	// }
+	// });
+
+	// }
+
+	private Dialog progressDialog;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
 		locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-		ImageButton btnGetHint = (ImageButton) this.findViewById(R.id.ibLogin);
-		btnGetHint.setOnClickListener(new View.OnClickListener() {
+		// Initialize the Parse Data Store
+		Log.w("test", getString(R.string.parseAppId));
+		Log.w("test", getString(R.string.parseClientKey));
+		Parse.initialize(this, getString(R.string.parseAppId),
+				getString(R.string.parseClientKey));
+
+		// Check if there is a currently logged in user
+		// and it's linked to a Facebook account.
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+			// Go to the user info activity
+			GoToHome();
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+	}
+
+	public void onLoginClick(View v) {
+		progressDialog = ProgressDialog.show(this, "", "Logging in...", true);
+
+		List<String> permissions = Arrays.asList("public_profile", "email");
+
+		ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
 			@Override
-			public void onClick(View v) {
-				Intent intObj = new Intent(getBaseContext(), HomeActivity.class);
-				startActivity(intObj);
+			public void done(ParseUser user, ParseException err) {
+				progressDialog.dismiss();
+				if (user == null) {
+					// Log.d(IntegratingFacebookTutorialApplication.TAG,
+					// "Uh oh. The user cancelled the Facebook login.");
+				} else if (user.isNew()) {
+					// Log.d(IntegratingFacebookTutorialApplication.TAG,
+					// "User signed up and logged in through Facebook!");
+					GoToHome();
+				} else {
+					// Log.d(IntegratingFacebookTutorialApplication.TAG,
+					// "User logged in through Facebook!");
+					GoToHome();
+				}
 			}
 		});
+	}
 
+	private void GoToHome() {
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
